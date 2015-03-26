a9158fd7b19e0c8fe5e44d56833d58bd27817382
Timothy Schonberger <flyingtoaster0@gmail.com>
2015-03-25 21:40:59 -0400

    Added Robolectric

---
+++ Bixe/src/test/java/com.flyingtoaster.bixe/BixeTestRunner.java
@@ -0,0 +1,28 @@
+package com.flyingtoaster.bixe;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.AndroidManifest;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.res.Fs;
+import org.robolectric.res.FsFile;
+
+public class BixeTestRunner extends RobolectricTestRunner {
+  public BixeTestRunner(Class<?> testClass) throws InitializationError {
+    super(testClass);
+  }
+
+  @Override
+  protected AndroidManifest getAppManifest(Config config) {
+    String myAppPath = BixeTestRunner.class.getProtectionDomain()
+                                                        .getCodeSource()
+                                                        .getLocation()
+                                                        .getPath();
+      String manifestPath = myAppPath + "../../../../../src/main/AndroidManifest.xml";
+
+      FsFile customManifest = Fs.fileFromPath(manifestPath);
+      FsFile appBaseDir = customManifest.getParent();
+
+    return new com.flyingtoaster.bixe.NoApplicationAndroidManifest(customManifest, appBaseDir.join("res"), appBaseDir.join("assets"));
+  }
+}
---
+++ Bixe/src/test/java/com.flyingtoaster.bixe/ContentProviderTest.java
@@ -0,0 +1,17 @@
+package com.flyingtoaster.bixe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+
+@RunWith(RobolectricTestRunner.class)
+public class ContentProviderTest {
+
+    @Test
+    public void stupidTest() {
+        assertThat("TEST").isEmpty();
+    }
+}
