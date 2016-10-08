a0dccfbbf3a8adddd8c204ea4387fe93fd6ade29
Tim Schonberger <flyingtoaster0@gmail.com>
2016-10-08 14:54:00 -0400

    Add LocalStationProvider

---
+++ Bixe/src/main/java/com/flyingtoaster/bixe/providers/LocalStationProvider.java
@@ -0,0 +1,33 @@
+package com.flyingtoaster.bixe.providers;
+
+
+import com.flyingtoaster.bixe.datasets.StationDataSource;
+import com.flyingtoaster.bixe.models.Station;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+
+public class LocalStationProvider {
+
+    private final StationDataSource mDataSource;
+
+    public LocalStationProvider(StationDataSource dataSource) {
+        mDataSource = dataSource;
+    }
+
+    public Observable<List<Station>> getStations() {
+        return Observable.create(new ObservableOnSubscribe<List<Station>>() {
+            @Override
+            public void subscribe(ObservableEmitter<List<Station>> e) throws Exception {
+                mDataSource.open();
+                List<Station> stations = mDataSource.getAllStations();
+                mDataSource.close();
+                e.onNext(stations);
+                e.onComplete();
+            }
+        });
+    }
+}
--- Bixe/src/test/java/com/flyingtoaster/bixe/providers/LocalStationProviderTest.java
+++ Bixe/src/test/java/com/flyingtoaster/bixe/providers/LocalStationProviderTest.java
@@ -1,52 +1,55 @@
 package com.flyingtoaster.bixe.providers;
 
+import com.flyingtoaster.bixe.BixeTestRunner;
+import com.flyingtoaster.bixe.datasets.StationDataSource;
 import com.flyingtoaster.bixe.models.Station;
+import com.google.android.collect.Lists;
 
 import org.fest.assertions.data.Index;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.Collections;
 import java.util.List;
-
-import com.flyingtoaster.bixe.clients.StationClient;
-import com.google.android.collect.Lists;
 
 import io.reactivex.Observable;
 import io.reactivex.observers.TestObserver;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class StationProviderTest {
+@RunWith(BixeTestRunner.class)
+public class LocalStationProviderTest {
 
     @Mock
-    private StationClient mClient;
+    StationDataSource mDataSource;
 
-    private StationProvider mSubject;
-
+    LocalStationProvider mSubject;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
-        mSubject = new StationProvider(mClient);
+        mSubject = new LocalStationProvider(mDataSource);
     }
 
     @Test
-    public void whenClientReturnsWithStations_onNextShouldReturnStations() {
+    public void getStationsShouldGetStationsFromDataSource() {
         Station station = mock(Station.class);
-        when(mClient.getStations()).thenReturn(Lists.newArrayList(station));
+        when(mDataSource.getAllStations()).thenReturn(Lists.newArrayList(station));
         TestObserver<List<Station>> observer = new TestObserver<>();
 
-        Observable<List<Station>> stationsObservable = mSubject.getStations();
-        stationsObservable.subscribe(observer);
+        Observable<List<Station>> observable = mSubject.getStations();
+        observable.subscribe(observer);
 
         List<List<Station>> emittedResponse = observer.values();
         assertThat(emittedResponse).isNotEmpty();
         List<Station> stationList = emittedResponse.get(0);
         assertThat(stationList).contains(station, Index.atIndex(0));
+        verify(mDataSource).open();
+        verify(mDataSource).close();
     }
 }
