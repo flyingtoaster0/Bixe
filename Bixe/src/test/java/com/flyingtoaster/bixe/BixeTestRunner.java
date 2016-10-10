6ba3e73978fe1c38ca5ab2bd9aba67eb410df5a3
Tim Schonberger <flyingtoaster0@gmail.com>
2016-10-10 10:50:36 -0400

    Change some stuff in BixeTestRunner… Do I need it?

--- Bixe/src/test/java/com/flyingtoaster/bixe/BixeTestRunner.java
+++ Bixe/src/test/java/com/flyingtoaster/bixe/BixeTestRunner.java
@@ -1,10 +1,10 @@
 package com.flyingtoaster.bixe;
 
 import org.junit.runners.model.InitializationError;
-import org.robolectric.manifest.AndroidManifest;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-import org.robolectric.res.Fs;
+import org.robolectric.manifest.AndroidManifest;
+import org.robolectric.res.FileFsFile;
 import org.robolectric.res.FsFile;
 
 public class BixeTestRunner extends RobolectricTestRunner {
@@ -12,17 +12,23 @@ public class BixeTestRunner extends RobolectricTestRunner {
     super(testClass);
   }
 
-  @Override
-  protected AndroidManifest getAppManifest(Config config) {
-    String myAppPath = BixeTestRunner.class.getProtectionDomain()
-                                                        .getCodeSource()
-                                                        .getLocation()
-                                                        .getPath();
-      String manifestPath = myAppPath + "../../../../../src/main/AndroidManifest.xml";
+    protected AndroidManifest getAppManifest(Config config) {
+        AndroidManifest appManifest = super.getAppManifest(config);
+        FsFile androidManifestFile = appManifest.getAndroidManifestFile();
 
-      FsFile customManifest = Fs.fileFromPath(manifestPath);
-      FsFile appBaseDir = customManifest.getParent();
+        if (androidManifestFile.exists()) {
+            return appManifest;
+        } else {
+            String moduleRoot = getModuleRootPath(config);
+            androidManifestFile = FileFsFile.from(moduleRoot, appManifest.getAndroidManifestFile().getPath().replace("bundles", "manifests/full"));
+            FsFile resDirectory = FileFsFile.from(moduleRoot, appManifest.getResDirectory().getPath().replace("/res", "").replace("bundles", "res"));
+            FsFile assetsDirectory = FileFsFile.from(moduleRoot, appManifest.getAssetsDirectory().getPath().replace("/assets", "").replace("bundles", "assets"));
+            return new AndroidManifest(androidManifestFile, resDirectory, assetsDirectory);
+        }
+    }
 
-    return new AndroidManifest(customManifest, appBaseDir.join("res"), appBaseDir.join("assets"));
-  }
+    private String getModuleRootPath(Config config) {
+        String moduleRoot = config.constants().getResource("").toString().replace("file:", "");
+        return moduleRoot.substring(0, moduleRoot.indexOf("/build"));
+    }
 }
